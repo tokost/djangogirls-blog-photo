6e5b923d13b32b103922b99a6cfaaafe4f7fc65e
Katie Jolly <kjolly@macalester.edu>
2017-09-04 15:46:25 -0500

    Modified tempates to display posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+

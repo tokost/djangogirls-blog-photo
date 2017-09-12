fa64741c30a025ffb814430414fd9385dac30242
Katie Jolly <kjolly@macalester.edu>
2017-09-12 14:51:04 -0500

    Added new features like login/logout

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,43 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
+# creates the list of posts on the homepage
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+#what you see when you click on a post
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
 
-def post_new(request):
-	form = PostForm()
-	return render(request, 'blog/post_edit.html', {'form': form})
+#creating a new post-- login required, I don't think I need this view anymore
+#since I have the one below it now! 
+# @login_required
+# def post_new(request):
+# 	form = PostForm()
+# 	return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required #decorator for securing the site, require login
 def post_new(request):
 	if request.method == "POST":
 		form= PostForm()
 		if form.is_valid():
 			post = form.save(commit = False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form':form})
 
+# edit a post that has already been written
+@login_required
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
@@ -37,11 +45,29 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit = False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
 
+# list of posts that haven't been published yet (drafts)
+@login_required
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts':posts})
 
+# manually publish a post
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+# delete a post
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
